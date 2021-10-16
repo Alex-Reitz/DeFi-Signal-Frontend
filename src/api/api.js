bc3d364b687d406c4d8398d4158ee4929211fbea
Alex Reitz <alex.j.reitz@gmail.com>
2021-10-16 05:33:53 -0400

    heroku url update

--- src/api/api.js
+++ src/api/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://defisignal-backend.herokuapp.com/";
+const BASE_URL = "https://defisignal-backend.herokuapp.com";
 
 class DeFiSignalApi {
   static token;
